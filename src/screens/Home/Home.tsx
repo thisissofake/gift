49ddce8bc315af2bffc9758b3d8c475280870857
Anton Ignatev <nsls@atamore.com>
2022-05-22 01:37:29 +0300

    Fixes

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 
+import { Layout } from 'components';
+
 import { Entry, Success } from './components';
 
 function Home() {
-  // const [codeEntered, setCodeEntered] = React.useState(false);
-  const [codeEntered, setCodeEntered] = React.useState(true);
+  const [codeEntered, setCodeEntered] = React.useState(false);
 
-  return codeEntered ? (
-    <Success />
-  ) : (
-    <Entry
-      onSuccess={() => {
-        setCodeEntered(true);
-      }}
-    />
+  return (
+    <Layout>
+      {codeEntered ? (
+        <Success />
+      ) : (
+        <Entry
+          onSuccess={() => {
+            setCodeEntered(true);
+          }}
+        />
+      )}
+    </Layout>
   );
 }
 
