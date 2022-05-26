e24f9901c507f35279f91ad16d3cab868f30290e
Anton Ignatev <nsls@atamore.com>
2022-05-26 15:05:44 +0300

    Gift info

--- src/screens/Home/components/Success/Success.tsx
+++ src/screens/Home/components/Success/Success.tsx
@@ -9,13 +9,23 @@ function Success() {
   return (
     <Wrapper>
       <Typography variant='h4' gutterBottom>
-        Твой подарок сейчас в пути!
+        Твой подарок ждёт тебя в пункте выдачи OZON по адресу Богатырский
+        проспект 60к1!
       </Typography>
+      <div style={{ margin: '0 auto 20px', display: 'table' }}>
+        <StaticImage alt='Карта' src='./assets/map.png' width={400} />
+      </div>
       <Typography variant='h4' gutterBottom>
-        Заходи сюда 26 мая в 18:00, будет интересно
+        Чтобы получить его, покажи работнику штрихкод ниже:
       </Typography>
 
-      <StaticImage alt='Опять ждать' src='./assets/zhdun.png' width={400} />
+      <div style={{ margin: '0 auto', display: 'table' }}>
+        <StaticImage alt='Штрихкод' src='./assets/barcode.jpg' width={300} />
+      </div>
+
+      <Typography style={{ textAlign: 'center' }} variant='h4' gutterBottom>
+        С днём рождения!
+      </Typography>
     </Wrapper>
   );
 }
